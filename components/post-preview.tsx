a57f7f26ae7b422111773f1bcda5e548fb65c9c8
plastic041@ <plastic041@gmail.com>
2021-08-19 21:53:30 +0900

    no use image in post-preview

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,4 +1,3 @@
-import CoverImage from './cover-image';
 import DateFormatter from './date-formatter';
 import Link from 'next/link';
 import React from 'react';
@@ -14,18 +13,13 @@ type Props = {
 const PostPreview = ({ title, coverImage, date, excerpt, slug }: Props) => {
   return (
     <Link as={`/posts/${slug}`} href="/posts/[slug]" passHref>
-      <a className="w-full lg:h-40 post-item lg:flex md:flex-row items-center mx-auto transition duration-100 shadow hover:shadow-lg rounded p-2 overflow-hidden">
-        <CoverImage
-          className="hidden md:block w-full lg:w-2/5 lg:block lg:mr-2"
-          title={title}
-          src={coverImage || '/assets/blog/preview/cover.jpg'}
-        />
-        <div className="lg:w-3/5 flex flex-col justify-center overflow-hidden">
-          <h3 className="text-2xl mb-1 ">{title}</h3>
+      <a className="w-full h-36 post-item flex items-center mx-auto transition duration-100 shadow hover:shadow-lg rounded-md py-2 px-4 overflow-hidden">
+        <div className="flex flex-col justify-center">
+          <h3 className="text-2xl mb-1">{title}</h3>
           <div className="text-sm mb-2">
             <DateFormatter dateString={date} />
           </div>
-          <p className="text-base truncate">{excerpt}</p>
+          <p className="text-base">{excerpt}</p>
         </div>
       </a>
     </Link>
