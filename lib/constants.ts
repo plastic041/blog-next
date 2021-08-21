d0d1d22eb22ec74ca304b44fae1d484b5f8f6862
plastic041@ <plastic041@gmail.com>
2021-08-21 23:37:52 +0900

    change og image

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,4 +2,4 @@ export const EXAMPLE_PATH = 'blog-starter-typescript';
 export const CMS_NAME = 'Markdown';
 export const BLOG_NAME = '스넙 블로그';
 export const HOME_OG_IMAGE_URL =
-  'https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg';
+  'https://og-image.vercel.apphttps://og-image.vercel.app/**SNUBI**.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-white.svg';
