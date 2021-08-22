2a0cdda629c4b65895b9087a7615fb9f0a55dcf7
plastic041@ <plastic041@gmail.com>
2021-08-22 13:55:02 +0900

    fix og image url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,5 @@
 export const EXAMPLE_PATH = 'blog-starter-typescript';
 export const CMS_NAME = 'Markdown';
 export const BLOG_NAME = '스넙 블로그';
-export const HOME_OG_IMAGE = '/assets/blog/og/ogimage.png';
+export const HOME_OG_IMAGE =
+  'https://snubi.netlify.app/assets/blog/og/ogimage.png';
