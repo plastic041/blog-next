b5b1596f829bc11a4036b670dfce9196cff6f655
plastic041 <plastic041@gmail.com>
2021-08-23 01:29:28 +0000

    모바일 글목록 페이지 레이아웃 깨짐 수정 Fixes #11

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -13,23 +13,21 @@ type Props = {
 const PostPreview = (post: Props) => {
   return (
     <Link as={`/posts/${post.slug}`} href="/posts/[slug]">
-      <a className="flex items-center w-full px-4 py-2 transition duration-100 h-32 post-item hover:shadow-lg">
-        <div className="flex flex-col justify-center">
-          <h3 className="mb-1 text-2xl">{post.title}</h3>
-          <div className="mb-2 text-sm">
-            <span className="mr-4">
-              {'작성: '}
-              <DateFormatter dateString={post.date} />
+      <a className="block flex flex-col justify-center keep-all w-full px-4 py-2 transition duration-100 h-32 post-item hover:shadow-lg">
+        <h3 className="mb-1 text-xl md:text-2xl">{post.title}</h3>
+        <div className="mb-2 text-xs md:text-sm">
+          <span className="mr-4">
+            {'작성: '}
+            <DateFormatter dateString={post.date} />
+          </span>
+          {post.modifiedDate && (
+            <span>
+              {'수정: '}
+              <DateFormatter dateString={post.modifiedDate} />
             </span>
-            {post.modifiedDate && (
-              <span>
-                {'수정: '}
-                <DateFormatter dateString={post.modifiedDate} />
-              </span>
-            )}
-          </div>
-          <p className="text-base">{post.description}</p>
+          )}
         </div>
+        <p className="text-sm md:text-base">{post.description}</p>
       </a>
     </Link>
   );
