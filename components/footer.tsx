2d85f46b9f817d3f8621d887e46a51a4402692fb
plastic041 <plastic041@gmail.com>
2021-08-26 03:31:10 +0000

    add footer

---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <footer className="py-4 text-base text-center italic">
+      <span>copyright © 스넙</span>
+    </footer>
+  );
+};
+
+export default Header;
--- components/header.tsx
+++ components/header.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <h2 className="pt-4 pb-20 text-4xl font-bold leading-tight tracking-tight md:text-6xl md:tracking-tighter">
-      <Link href="/">
-        <a className="hover:underline">스넙</a>
-      </Link>
-    </h2>
+    <header>
+      <h2 className="pt-4 pb-20 text-4xl font-bold md:text-6xl">
+        <Link href="/">
+          <a className="hover:underline">스넙</a>
+        </Link>
+      </h2>
+    </header>
   );
 };
 
