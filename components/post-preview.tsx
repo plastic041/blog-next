069b686b25edc179a1f15f2b52e79609c9e1bb86
plastic041@ <plastic041@gmail.com>
2021-08-26 17:43:48 +0900

    fix css

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -13,7 +13,7 @@ type Props = {
 const PostPreview = (post: Props) => {
   return (
     <Link as={`/posts/${post.slug}`} href="/posts/[slug]">
-      <a className="flex flex-col justify-center block w-full h-32 px-4 py-2 transition duration-100 keep-all post-item hover:shadow-lg">
+      <a className="flex flex-col justify-center w-full h-32 px-4 py-2 transition duration-100 keep-all post-item hover:shadow-lg">
         <h3 className="mb-1 text-xl md:text-2xl">{post.title}</h3>
         <div className="mb-2 text-xs md:text-sm">
           <span className="mr-4">
