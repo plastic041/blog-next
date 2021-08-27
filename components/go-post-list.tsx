568f404926321074e8f4fac14db2fd036aa80df9
plastic041 <plastic041@gmail.com>
2021-08-27 00:42:51 +0000

    link hover css

--- components/go-post-list.tsx
+++ components/go-post-list.tsx
@@ -5,7 +5,9 @@ const GoPostList = () => {
   return (
     <>
       <Link as={'/'} href="/" passHref>
-        <a className="block mr-4 text-right underline">글 목록으로 돌아가기</a>
+        <a className="block mr-4 text-right underline hover:text-blue-500">
+          글 목록으로 돌아가기
+        </a>
       </Link>
     </>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header>
       <h2 className="xl:px-40 pt-4 pb-20 text-4xl font-bold md:text-6xl">
         <Link href="/">
-          <a className="hover:underline">스넙</a>
+          <a className="hover:text-blue-500">스넙</a>
         </Link>
       </h2>
     </header>
