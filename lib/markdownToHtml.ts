83d8bdc6961277b08ab63f717e177c6852364475
plastic041@ <plastic041@gmail.com>
2021-08-28 16:32:02 +0900

    use unified

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,30 @@
-import html from 'remark-html';
-import remark from 'remark';
+import hast from 'hast';
+import rehypeStringify from 'rehype-stringify';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
+import { visit } from 'unist-util-visit';
 
-export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+function rehypeImgLazyLoad() {
+  return (tree: hast.Node) => {
+    visit(tree, 'element', (node: hast.Element) => {
+      if (node.tagName === 'img') {
+        node.properties = {
+          ...node.properties,
+          loading: 'lazy',
+        };
+      }
+    });
+  };
+}
+
+const processor = unified()
+  .use(remarkParse)
+  .use(remarkRehype)
+  .use(rehypeImgLazyLoad)
+  .use(rehypeStringify);
+
+export default function markdownToHtml(markdown: string) {
+  const result = processor.processSync(markdown);
   return result.toString();
 }
